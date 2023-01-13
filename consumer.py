c2abb78aa49aed552d7127b86276bf4a73130757
JoshHarris2108 <114068472+JoshHarris2108@users.noreply.github.com>
2023-01-13 13:33:33 +0000

    Create consumer.py
    Gets filled data buffers, appends them to a dataset, resets the buffer back to empty and then sets that buffer ready to be written into

---
+++ consumer.py
@@ -0,0 +1,70 @@
+import queue
+import h5py
+import time
+import numpy as np
+
+class Consumer:
+    def __init__(self, buffer_size, data_queue, empty_queue, data_buffers,file_name):
+        self.buffer_size = buffer_size
+        self.data_queue = data_queue
+        self.empty_queue = empty_queue
+        self.data_buffers = data_buffers
+        self.file_name = file_name
+        self.running = True
+
+        # To keep track of how much the dataset needs to be resized
+        self.values_written = 0
+
+        self.cmaxSamples = 0
+        self.timeIntervalns = 0
+        self.chARange = 0
+        self.maxADC = 0
+
+    def set_metadata(self,cmaxSamples,timeIntervalns,chARange,maxADC):
+        self.cmaxSamples = cmaxSamples
+        self.timeIntervalns = timeIntervalns
+        self.chARange = chARange
+        self.maxADC = maxADC
+
+    def stop(self):
+        self.running = False
+    
+    def consume(self):
+        total_save_length = 0
+        metadata = {
+            'cmaxSamples': 25600,
+            'timeIntervalns': 80,
+            'chARange': self.chARange,
+            'maxADC': self.maxADC
+        }
+        with h5py.File('/tmp/data.hdf5','w') as f:
+            metadata_group = f.create_group('metadata')
+            for key, value in metadata.items():
+                metadata_group.attrs[key] = value
+
+            #dset = f.create_dataset('adc_counts',(self.buffer_size,),maxshape=(None,),dtype='int16', chunks=True,data = np.ones((30,)))
+            dset = f.create_dataset('adc_counts',(self.buffer_size,),maxshape=(None,),dtype='int16', chunks=True)
+            #dset[0] = np.ones((30,), dtype='int16')
+
+            while self.running:
+                try:
+                    # if self.data_buffers[idx].size != self.buffer_size 
+                        # resize dataset to self.data_buffers[idx].size
+    
+                    idx = self.data_queue.get(timeout=0.1)
+                    print("consumer got data idx", idx)
+
+                    dset.resize((self.values_written+self.buffer_size,))
+                    dset[self.values_written:] = self.data_buffers[idx]
+
+                    self.values_written += self.data_buffers[idx].size
+                    self.data_buffers[idx] = np.empty(0,dtype='int16')
+                    self.empty_queue.put(idx)
+
+                    #print("\n\nData in consumer: ",self.data_buffers[idx])
+                except queue.Empty:
+                    print("data_queue empty")
+                    pass
+                
+                 
+        print("Data saving finished", total_save_length)
