ec559479ca9a7a7d9641c23865b167cd22df1bdd
JoshHarris2108 <114068472+JoshHarris2108@users.noreply.github.com>
2023-01-19 10:58:39 +0000

    Update main.py
    A messy update to main.py, a picodevice object is created, the setup for the picoscope is performed, and the picoscope is told to start collecting data

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import queue
 import signal
 import threading
+import ctypes
 #import time
 import numpy as np
 
 from producer import Producer
 from consumer import Consumer
+from pico import PicoDevice
 
 
 class StreamExample():
@@ -14,38 +16,57 @@ class StreamExample():
 
         data_queue = queue.Queue()
         empty_queue = queue.Queue()
-        buffer_size = 30
-        num_buffers = 3
+        buffer_size = 25600
+        num_buffers = 120
         data_buffers = []
-        file_name = 'data.npy' #not in use atm
+        auto_stop = 1
+        auto_stop_stream = False
+        # buffer_size and num_buffers need to be moved elsewhere 
+        self.pico_device = PicoDevice(0,"PS5000A_DR_12BIT",buffer_size, num_buffers)
+        self.pico_device.set_channel('setChA','PS5000A_CHANNEL_A',1,'PS5000A_DC','PS5000A_20V',0.0)
+        self.pico_device.set_data_buffer('setDataBufferA','PS5000A_CHANNEL_A',0,'PS5000A_RATIO_MODE_NONE')
+        # call a funciton that sets the run_streaming variables up, but doesnt actually call the 
+        # run streaming 
+        self.pico_device.configure_streaming(32,'PS5000A_NS',0,1,'PS5000A_RATIO_MODE_NONE',auto_stop, auto_stop_stream)
+        
+
+
+
+
+        file_name = 'data.npy' #not in use
 
         # Creates an empty_queue that stores the indexes of empty buffers
         # The buffers are created as empty buffers and all added to the empty 
         # queue ready to be filled with data by the producer
         for idx in range(num_buffers):
-            data_buffers.append(np.empty(0,dtype='int16'))
+            data_buffers.append(np.empty((buffer_size,), dtype='int16'))
             empty_queue.put(idx)
         
-        self.producer = Producer(buffer_size,data_queue,empty_queue,data_buffers)
-        self.consumer = Consumer(buffer_size,data_queue,empty_queue,data_buffers,file_name)
+        #self.producer = Producer(buffer_size,data_queue,empty_queue,data_buffers)
+        #self.consumer = Consumer(buffer_size,data_queue,empty_queue,data_buffers,file_name)
+        self.pico_thread = threading.Thread(target=self.pico_device.callback_loop)
 
-        self.producer_thread = threading.Thread(target=self.producer.produce)
-        self.consumer_thread = threading.Thread(target=self.consumer.consume)
+        #self.producer_thread = threading.Thread(target=self.producer.produce)
+        #self.consumer_thread = threading.Thread(target=self.consumer.consume)
 
         signal.signal(signal.SIGINT, self.signal_handler)
 
     def signal_handler(self, sig, frame):
         print("Shutting down")
-        self.producer.stop()
-        self.consumer.stop()
+        #self.producer.stop()
+        #self.consumer.stop()
+        self.pico_device.running = False
+        self.pico_device.close_device()
 
     def run(self):
 
-        self.producer_thread.start()
-        self.consumer_thread.start()
+        #self.producer_thread.start()
+        #self.consumer_thread.start()
 
-        self.producer_thread.join()
-        self.consumer_thread.join()
+        #self.producer_thread.join()
+        #self.consumer_thread.join()
+        self.pico_thread.start()
+        self.pico_thread.join()
 
 
 if __name__ == '__main__':
