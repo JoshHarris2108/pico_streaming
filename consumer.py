5ad30ea724d68141cd66151efa39e2a19112d4e8
JoshHarris2108 <114068472+JoshHarris2108@users.noreply.github.com>
2023-02-07 13:01:37 +0000

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -12,13 +12,14 @@ class Consumer:
         self.file_name = file_name
         self.running = True
 
-        # To keep track of how much the dataset needs to be resized
         self.values_written = 0
 
         self.cmaxSamples = 0
         self.timeIntervalns = 0
         self.chARange = 0
         self.maxADC = 0
+
+        self.empty_con_queue_count = 0
 
     def set_metadata(self,cmaxSamples,timeIntervalns,chARange,maxADC):
         self.cmaxSamples = cmaxSamples
@@ -32,7 +33,7 @@ class Consumer:
     def consume(self):
         total_save_length = 0
         metadata = {
-            'cmaxSamples': 25600,
+            'cmaxSamples': 2560,
             'timeIntervalns': 80,
             'chARange': self.chARange,
             'maxADC': self.maxADC
@@ -42,29 +43,21 @@ class Consumer:
             for key, value in metadata.items():
                 metadata_group.attrs[key] = value
 
-            #dset = f.create_dataset('adc_counts',(self.buffer_size,),maxshape=(None,),dtype='int16', chunks=True,data = np.ones((30,)))
-            dset = f.create_dataset('adc_counts',(self.buffer_size,),maxshape=(None,),dtype='int16', chunks=True)
-            #dset[0] = np.ones((30,), dtype='int16')
+            dset = f.create_dataset('adc_counts',(self.buffer_size,),maxshape=(None,),dtype='int16', chunks=(self.buffer_size,))
 
             while self.running:
+                if not self.running:
+                    break
                 try:
-                    # if self.data_buffers[idx].size != self.buffer_size 
-                        # resize dataset to self.data_buffers[idx].size
-    
                     idx = self.data_queue.get(timeout=0.1)
-                    print("consumer got data idx", idx)
 
-                    dset.resize((self.values_written+self.buffer_size,))
+                    dset.resize((self.values_written+(len(self.data_buffers[idx])),))
                     dset[self.values_written:] = self.data_buffers[idx]
-
-                    self.values_written += self.data_buffers[idx].size
-                    self.data_buffers[idx] = np.empty(0,dtype='int16')
                     self.empty_queue.put(idx)
 
-                    #print("\n\nData in consumer: ",self.data_buffers[idx])
+                    self.values_written += len(self.data_buffers[idx])
+
                 except queue.Empty:
-                    print("data_queue empty")
-                    pass
-                
-                 
-        print("Data saving finished", total_save_length)
+                    self.empty_con_queue_count += 1   
+
+        print("Number of times consumer couldnt obtain queue: ", self.empty_con_queue_count)
